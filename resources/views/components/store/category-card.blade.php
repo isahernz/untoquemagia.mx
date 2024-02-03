8417537f66c48d29c868cf7fdb13e93fab0eff42
Brandon Isahir Hernández Camacho <isahernz@outlook.com>
2024-02-03 15:07:54 -0600

    feat: :sparkles: add category card component

---
+++ resources/views/components/store/category-card.blade.php
@@ -0,0 +1,16 @@
+<a
+  class="flex w-full overflow-hidden rounded-xl bg-magic-50 sm:max-w-max md:max-w-md"
+  href="#"
+>
+  <div class="flex w-3/5 flex-col justify-center gap-3 p-6">
+    <span class="text-xs font-bold uppercase tracking-wider text-magic-600 xs:text-sm">{{ $smallTitle }}</span>
+    <h3 class="text-balance text-xl font-extrabold uppercase text-magic-800 xs:text-2xl">{{ $title }}</h3>
+    <button class="h-9 w-28 rounded-lg bg-magic-500 text-white">
+      <span class="text-sm">{{ $button }}</span></button>
+  </div>
+  <img
+    class="w-2/5 object-cover object-center"
+    src="https://images1.novica.net/pictures/27/p445910_2_400.jpg"
+    alt="Pulseras tejidas"
+  >
+</a>
