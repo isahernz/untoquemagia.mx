8559a9d3d11aa2597fc0ba577fc07ef46ca0ebcb
Brandon Isahir Hernández Camacho <isahernz@outlook.com>
2024-02-02 18:12:26 -0600

    feat: :sparkles: add category link component and nav container

---
+++ resources/views/components/link-category.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => 'text-base font-semibold text-magic-700 w-full min-w-max']) }}>
+  {{ $text }}
+</a>
---
+++ resources/views/components/nav-category.blade.php
@@ -0,0 +1,35 @@
+<nav class="flex items-center gap-4 overflow-x-auto px-4 pb-4">
+  <x-link-category
+    class="rounded-full border border-[#e87ea4] px-3 py-1"
+    href="#"
+    :text="'All'"
+  />
+  <x-link-category
+    href="#"
+    :text="'Dulces Típicos'"
+  />
+  <x-link-category
+    href="#"
+    :text="'Artesanías'"
+  />
+  <x-link-category
+    href="#"
+    :text="'Talavera'"
+  />
+  <x-link-category
+    href="#"
+    :text="'Ropa'"
+  />
+  <x-link-category
+    href="#"
+    :text="'Juguetes'"
+  />
+  <x-link-category
+    href="#"
+    :text="'Licores'"
+  />
+  <x-link-category
+    href="#"
+    :text="'Artículos de temporada'"
+  />
+</nav>
